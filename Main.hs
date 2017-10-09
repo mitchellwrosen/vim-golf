458275d67a486b81b0350525fcdfaa70f09b9f54
Mitchell Rosen <mitchellwrosen@gmail.com>
2017-10-09 17:55:36 -0400

    Add some simple buffer movement and insertion

--- Main.hs
+++ Main.hs
@@ -1,15 +1,19 @@
+{-# language RecordWildCards #-}
+
 module Main where
 
 import Control.Exception (finally)
 import Data.IORef
-import Graphics.Vty (Key(..), Modifier(..), Picture, Vty)
+import Graphics.Vty (Image, Key(..), Modifier(..), Picture, Vty)
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 
 import qualified Graphics.Vty as Vty
 
 data Vim = Vim
-  { vimMode :: Mode
+  { vimMode      :: !Mode
+  , vimCursor    :: !Int
+  , vimBuffer    :: !String
   } deriving Show
 
 data Mode
@@ -67,11 +71,22 @@ vimGolf eKey = mdo
   let eEsc :: Event ()
       eEsc = () <$ filterE ((== KEsc) . fst) eKey
 
+  let eMode :: Event Mode
+      eMode =
+        foldr (unionWith const) never
+          [ NormalMode <$ whenE bIsInsertMode eEsc
+          , InsertMode <$
+              whenE bIsNormalMode (filterE (== (KChar 'i', [])) eKey)
+          ]
+
+  let eNormalMode :: Event ()
+      eNormalMode = () <$ filterE (== NormalMode) eMode
+
+  let eInsertMode :: Event ()
+      eInsertMode = () <$ filterE (== InsertMode) eMode
+
   bMode :: Behavior Mode <-
-    stepper NormalMode (foldr (unionWith const) never
-      [ NormalMode <$ whenE bIsInsertMode eEsc
-      , InsertMode <$ whenE bIsNormalMode (filterE (== (KChar 'i', [])) eKey)
-      ])
+    stepper NormalMode eMode
 
   let bIsNormalMode :: Behavior Bool
       bIsNormalMode = (== NormalMode) <$> bMode
@@ -79,15 +94,119 @@ vimGolf eKey = mdo
   let bIsInsertMode :: Behavior Bool
       bIsInsertMode = (== InsertMode) <$> bMode
 
+  eMoveLeft :: Event () <- do
+    let eKeyH :: Event ()
+        eKeyH = () <$ filterE (== (KChar 'h', [])) eKey
+
+    let eKeyLeft :: Event ()
+        eKeyLeft = () <$ filterE (== (KLeft, [])) eKey
+
+    let eNormalMoveLeft :: Event ()
+        eNormalMoveLeft = eKeyH <> eKeyLeft
+
+    let eInsertMoveLeft :: Event ()
+        eInsertMoveLeft = eKeyLeft
+
+    switchE eNormalMoveLeft
+      (foldr (unionWith const) never
+        [ eNormalMoveLeft <$ eNormalMode
+        , eInsertMoveLeft <$ eInsertMode
+        ])
+
+  eMoveRight :: Event () <- do
+    let eKeyL :: Event ()
+        eKeyL = () <$ filterE (== (KChar 'l', [])) eKey
+
+    let eKeyRight :: Event ()
+        eKeyRight = () <$ filterE (== (KRight, [])) eKey
+
+    let eNormalMoveRight :: Event ()
+        eNormalMoveRight = eKeyL <> eKeyRight
+
+    let eInsertMoveRight :: Event ()
+        eInsertMoveRight = eKeyRight
+
+    switchE eNormalMoveRight
+      (foldr (unionWith const) never
+        [ eNormalMoveRight <$ eNormalMode
+        , eInsertMoveRight <$ eInsertMode
+        ])
+
+  bCursor :: Behavior Int <- do
+    let inc :: Mode -> String -> Int -> Int
+        inc NormalMode buffer = min (length buffer - 1) . (+1)
+        inc InsertMode buffer = min (length buffer) . (+1)
+
+        dec :: Int -> Int
+        dec = max 0 . subtract 1
+
+    accumB 0 (unions
+      [ inc <$> bMode <*> bBuffer <@ eMoveRight
+      , inc <$> bMode <*> bBuffer <@ eInsertChar
+      , dec <$ eMoveLeft
+      , dec <$ whenE bIsInsertMode eNormalMode
+      ])
+
+  eInsertChar :: Event Char <- do
+    let eChar :: Event Char
+        eChar = filterJust (f <$> eKey)
+         where
+          f :: (Key, [Modifier]) -> Maybe Char
+          f (KChar c, []) = Just c
+          f _ = Nothing
+
+    switchE never
+      (foldr (unionWith const) never
+        [ eChar <$ eInsertMode
+        , never <$ eNormalMode
+        ])
+
+  bBuffer :: Behavior String <- do
+    accumB "foobar" (unions
+      [ insertAt <$> bCursor <@> eInsertChar
+      ])
+
   let bVim :: Behavior Vim
-      bVim = Vim
-        <$> bMode
+      bVim = do
+        vimMode   <- bMode
+        vimCursor <- bCursor
+        vimBuffer <- bBuffer
+        pure (Vim{..})
 
   let bPicture :: Behavior Picture
-      bPicture = Vty.picForImage . Vty.string Vty.defAttr . show <$> bVim
+      bPicture = Vty.picForImage . draw <$> bVim
+       where
+        draw :: Vim -> Image
+        draw vim = Vty.vertCat
+          [ Vty.string Vty.defAttr (show vim)
+          , drawBuffer
+          ]
+         where
+          drawBuffer :: Image
+          drawBuffer = Vty.horizCat (map drawChar (zip [0..] (vimBuffer vim)))
+
+          drawChar :: (Int, Char) -> Image
+          drawChar (i, c) = Vty.char attr c
+           where
+            attr =
+              if i == vimCursor vim
+              then
+                Vty.defAttr
+                  `Vty.withForeColor` Vty.black
+                  `Vty.withBackColor` Vty.white
+              else Vty.defAttr
 
   -- For now, pressing <Esc> while in normal mode exits vim (ha).
   let eDone :: Event ()
       eDone = whenE bIsNormalMode eEsc
 
   pure (bPicture, eDone)
+
+insertAt :: Int -> a -> [a] -> [a]
+insertAt 0 x xs = x:xs
+insertAt n x (y:ys) = y : insertAt (n-1) x ys
+
+safeTail :: [a] -> [a]
+safeTail = \case
+  []   -> []
+  _:xs -> xs
